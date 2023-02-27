0908fe185385d708ca9a12ae74ec006e1c7fbd47
FREEWING-JP <freewing.jp@gmail.com>
2023-02-27 18:33:14 +0900

    mod Add Log Output to Retrieve Image Bitmap to PNG format file C# tool

---
+++ retrieve_bitmap/LogDataToPngImage.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+
+namespace LogDataToPngImage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (FileStream dest = new FileStream("image_data.bin", FileMode.Create, FileAccess.Write))
+            {
+                foreach (string line in File.ReadLines(@"log_data.txt"))
+                {
+                    if (!line.StartsWith(" 0x")) continue;
+
+                    string hexValues = line.Replace(" 0x", "");
+                    hexValues = hexValues.Replace(",", " ");
+                    hexValues = hexValues.Trim();
+                    string[] hexValuesSplit = hexValues.Split(' ');
+                    foreach (string hex in hexValuesSplit)
+                    {
+                        byte value = Convert.ToByte(hex, 16);
+                        dest.WriteByte(value);
+                    }
+                }
+            }
+
+            int frame = 1;
+            const int BUFSIZE = 128 / 8 * 64;
+            byte[] buf = new byte[BUFSIZE];
+            using (FileStream src = new FileStream("image_data.bin", FileMode.Open, FileAccess.Read))
+            {
+                while (true)
+                {
+                    int readSize;
+                    try
+                    {
+                        readSize = src.Read(buf, 0, BUFSIZE);
+                    }
+                    catch
+                    {
+                        continue;
+                    }
+                    if (readSize == 0)
+                    {
+                        break;
+                    }
+
+                    Bitmap bmp = new Bitmap(128, 64);
+
+                    for (int y = 0; y < 64; ++y)
+                    {
+                        for (int x = 0; x < 16; ++x)
+                        {
+                            byte c = buf[x + y * 16];
+                            for (int b = 0; b < 8; ++b)
+                            {
+                                Color col = ((0x80 >> b) & c) == 0 ? Color.Black: Color.White;
+                                bmp.SetPixel((x*8)+b, y, col);
+                            }
+                        }
+                    }
+
+                    string fn = String.Format(@".\scale\{0:D5}.png", frame++);
+                    bmp.Save(fn, System.Drawing.Imaging.ImageFormat.Png);
+
+                    bmp.Dispose();
+                }
+            }
+        }
+    }
+}
+
---
+++ retrieve_bitmap/SerialComPort.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.IO;
+using System.Threading;
+
+namespace SerialComPort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // SerialPort.GetPortNames Method
+            // https://learn.microsoft.com/en-us/dotnet/api/system.io.ports.serialport.getportnames?view=netframework-4.8
+            // Get a list of serial port names.
+            string[] ports = SerialPort.GetPortNames();
+
+            Console.WriteLine("The following serial ports were found:");
+
+            // Display each port name to the console.
+            foreach (string port in ports)
+            {
+                Console.WriteLine(port);
+            }
+
+            Console.ReadLine();
+
+            // Logging
+            SerialPort sp = new SerialPort();
+            sp.PortName = "COM7";
+            sp.BaudRate = 921600;
+            sp.Parity = Parity.None;
+            sp.DataBits = 8;
+            sp.StopBits = StopBits.One;
+            sp.Handshake = Handshake.None;
+
+            try
+            {
+                sp.Open();
+
+                // RTS = Reset
+                sp.RtsEnable = true;
+                Thread.Sleep(1000);
+                sp.RtsEnable = false;
+
+                // sp.DtrEnable = true;
+                // Thread.Sleep(1000);
+                // sp.DtrEnable = false;
+
+                // set read timeout
+                sp.ReadTimeout = 5000;
+            }
+            catch (System.Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+
+            while (true)
+            {
+                File.AppendAllText(@"log_data.txt", sp.ReadLine());
+            }
+        }
+    }
+}
+
