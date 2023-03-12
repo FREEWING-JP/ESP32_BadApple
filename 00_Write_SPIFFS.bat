b148495de58fec4bc239722f513341cbb1f50471
FREEWING-JP <freewing.jp@gmail.com>
2023-03-12 16:29:11 +0900

    improve SPIFFS batch script

--- 00_Write_SPIFFS.bat
+++ 00_Write_SPIFFS.bat
@@ -1,13 +1,34 @@
 @echo off
 echo Change COM7 value to Your Environment
 echo Flash Size = 4MB, Partition Scheme = No OTA(1MB APP/ 3MB SPIFFS)
+echo ESP32 Board manager Arduino ESP32 Version 2.0.7
+
+if not exist %LocalAppData%\Arduino15\packages\esp32\ echo === Error === & exit /b
+
+if exist SPIFFS.bin del SPIFFS.bin
+
 rem %LocalAppData% = C:\Users\{User Name}\AppData\Local
-%LocalAppData%\Arduino15\packages\esp32\tools\mkspiffs\0.2.3\mkspiffs.exe ^
+rem %LocalAppData%\Arduino15\packages\esp32\tools\mkspiffs\0.2.3\mkspiffs.exe
+setlocal
+pushd %LocalAppData%\Arduino15\packages\esp32\tools\mkspiffs\
+for /f "usebackq delims=" %%A in (`dir /S /A-D /B mkspiffs.exe`) do set mkspiffs=%%A
+popd
+echo %mkspiffs%
+%mkspiffs% ^
   -c .\data ^
   -s 0x2e0000 ^
   SPIFFS.bin
 
-%LocalAppData%\Arduino15\packages\esp32\tools\esptool_py\4.2.1\esptool.exe ^
+if not exist SPIFFS.bin echo === Error === & exit /b
+
+dir SPIFFS.bin
+
+rem %LocalAppData%\Arduino15\packages\esp32\tools\esptool_py\4.5\esptool.exe
+pushd %LocalAppData%\Arduino15\packages\esp32\tools\esptool_py\
+for /f "usebackq delims=" %%A in (`dir /S /A-D /B esptool.exe`) do set esptool=%%A
+popd
+echo %esptool%
+%esptool% ^
   -p COM7 ^
   -b 921600 ^
   write_flash ^
@@ -15,3 +36,4 @@ rem %LocalAppData% = C:\Users\{User Name}\AppData\Local
   -fm dio ^
   0x00110000 ^
   SPIFFS.bin
+
